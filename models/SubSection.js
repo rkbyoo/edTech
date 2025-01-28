a42d962798bde7cf18cc91d79a86c11375ce6526
rakib hussain <rakibhussain8812@gmail.com>
2025-01-28 06:31:47 +0530

    adding models

--- models/SubSection.js
+++ models/SubSection.js
@@ -1,6 +1,18 @@
 const mongoose=require("mongoose")
 const subSectionSchema=mongoose.Schema({
- 
+    title:{
+        type:String,
+    },
+    timeDuration:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    videoUrl:{
+        type:String
+    }
+
 
 })
 

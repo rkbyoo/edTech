2a4bd4c1ab93573495cf17ee661e9ae4f4e11603
rakib hussain <rakibhussain8812@gmail.com>
2025-02-01 14:06:45 +0530

    g

--- controllers/authN.js
+++ controllers/authN.js
@@ -1,7 +1,53 @@
+const User=require("../models/User")
+const OTP=require("../models/OTP")
+const otpGenerator=require("otp-generator")
+
 //otp send
+exports.sendOTP=async(req,res)=>{
+    try {
+        const {email}=req.body
+    const userExist=await User.findOne({email})
+    if(userExist){
+        return res.status(403).json({
+            success:false,
+            message:"user already exists"
+        })
+    }
+    //using otp generator here to generate otps
+    let otp=otpGenerator.generate(6,{
+        upperCaseAlphabets:false,
+        lowerCaseAlphabets:false,
+        specialChars:false,
+    })
+    //checking if the generated otp is unique or not
+    let OtpExists=await OTP.findOne({otp:otp})
+    while(OtpExists)
+    {
+        otp=otpGenerator.generate(6,{
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+            specialChars:false,
+        })
+        OtpExists=await OTP.findOne({otp:otp})
+    }
+    //saving the OTP in the database
+    const OtpPayload={email,otp}
+    const otpBody=await OTP.create(OtpPayload)
+    console.log(otpBody)
 
+    return res.status(200).json({
+        success:true,
+        message:"OTP sent successfully"
+    })
 
-
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            success:false,
+            message:"error occured while sending OTP"
+        })
+    } 
+}
 
 
 

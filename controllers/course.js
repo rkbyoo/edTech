12c9e80ce4e39020e9217b2f1d255fcf6c2cab73
rakib hussain <rakibhussain8812@gmail.com>
2025-02-09 21:41:19 +0530

    complex

--- controllers/course.js
+++ controllers/course.js
@@ -8,7 +8,7 @@ const {uploadToCloudinary}=require("../utils/imageUploader")
 exports.createCourse=async(req,res)=>{
     try {
         //data fetch 
-        const {courseName,courseDescription,whatYouWillLearn,price,tag}=req.body
+        const {courseName,courseDescription,whatYouWillLearn,price,tag}=req.body //here the tag id is in req body
         //get thumbnail
         const thumbnail=req.files.thumbnailImage
         //validation
@@ -29,9 +29,52 @@ exports.createCourse=async(req,res)=>{
             })
         }
 
+        //check given tag is valid or not
+        const tagDetails=await Tag.findById({tag})
+        if(!tagDetails)
+        {
+            return res.status(404).json({
+                success:false,
+                message:"Tag is not found/Invalid"
+            })
+        }
+
+        //upload image to cloudinary
+        const thumbnailImage=await uploadToCloudinary(thumbnail,process.env.FOLDER_NAME)
+
+        //create an entry for new course
+        const newCourse=await Course.create({
+            courseName,
+            courseDescription,
+            instructor:instructorDetails._id,
+            whatYouWillLearn,
+            price,
+            tag:tagDetails._id,
+            thumbnail:thumbnailImage.secure_url
+        })
+
+        //add the new user to the user schema of instructor
+        await User.findByIdAndUpdate(
+            {_id:instructorDetails._id},
+            {$push:{courses:newCourse._id}}
+            ,{new:true}
+        )
+        //update the tag schema
+        await Tag.findByIdAndUpdate({tag},{$push:{course:newCourse._id}},{new:true})
+        
+        //return res
+        return res.status(200).json({
+            success:true,
+            message:"Course Created successfully",
+            data:newCourse
+        })
 
     } catch (error) {
-        
+        console.error("some error while creating course",error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error while creating course"
+        })
     }
 }
 

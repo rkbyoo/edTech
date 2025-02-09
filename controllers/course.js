c21263adff0b2b892e9742f6b11371171e5e870c
rakib hussain <rakibhussain8812@gmail.com>
2025-02-09 21:16:40 +0530

    g

--- controllers/course.js
+++ controllers/course.js
@@ -7,10 +7,32 @@ const {uploadToCloudinary}=require("../utils/imageUploader")
 
 exports.createCourse=async(req,res)=>{
     try {
-        
+        //data fetch 
+        const {courseName,courseDescription,whatYouWillLearn,price,tag}=req.body
+        //get thumbnail
+        const thumbnail=req.files.thumbnailImage
+        //validation
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !thumbnail){
+            return res.status(401).json({
+                success:false,
+                message:"Please fill up all the details"
+            })
+        }
+        //check for instructor 
+        const userId=req.user.id
+        const instructorDetails=await User.findById({userId})
+        console.log("instructor Details",instructorDetails)
+        if(!instructorDetails){
+            return res.status(404).json({
+                success:false,
+                message:"Instructor details not found"
+            })
+        }
+
+
     } catch (error) {
         
     }
 }
 
-//get al course
+//get all course

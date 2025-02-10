5e21de5553aead6015909b0b73ea57cc110798a5
rakib hussain <rakibhussain8812@gmail.com>
2025-02-10 09:55:57 +0530

    g

--- controllers/subsection.js
+++ controllers/subsection.js
@@ -2,6 +2,8 @@ const Subsection=require("../models/SubSection")
 const Section=require("../models/Section")
 const { uploadToCloudinary } = require("../utils/imageUploader")
 const Section = require("../models/Section")
+const { findById } = require("../models/Category")
+const SubSection = require("../models/SubSection")
 
 
 //create subsection
@@ -41,5 +43,70 @@ exports.createSubsection=async(req,res)=>{
 }
 
 //update subsection
+exports.updateSubSection=async(req,res)=>{
+    try {
+         //data fetch 
+    const {subSectionId,title,timeDuration,description}=req.body
+    //data validate 
+    if(!subSectionId ||!title||!timeDuration||!description){
+        return res.status(404).json({
+            success:false,
+            message:"unable to get required fields"
+        })
+    }
+    const subSection=findById({subSectionId})
+    if(!subSection){
+        return res.status(404).json({
+            success:false,
+            message:"unable to get the subsection"
+        })}
+    //data update
+    const updatedSubSection=await Subsection.findByIdAndUpdate({subSectionId},{title:title,timeDuration:timeDuration,description:description},{new:true})
+    //return res
+    return res.status(200).json({
+        success:true,
+        message:"subsection created successfully",
+        data:updatedSubSection
+    })
+    } catch (error) {
+        console.error("some error while updating subsection",error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error while updating the subsection"
+        })
+    }
+   
+}
+
 
 //delete subsection
+
+exports.deleteSubSection=async(req,res)=>{
+
+try {
+    //fetch id
+const {subSectionId}=req.params
+//validation
+if(!subSectionId){
+    return res.status(404).json({
+        success:false,
+        message:"unable to find the file id"
+    })
+}
+//delete
+await Subsection.findByIdAndDelete({subSectionId},{new:true})
+//update section schema
+await Section.findByIdAndUpdate({subSectionId},{$pull:{SubSection:subSectionId}})
+//return res
+return res.status(200).json({
+    success:true,
+    message:"successfully deleted the subsection"
+})
+} catch (error) {
+    console.error("some error while deleting subsection",error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error while deleting the subsection"
+        })
+}
+}

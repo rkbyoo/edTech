3c6d53eb1aed99bb3ffa75269429f7de8cbf47d6
rakib hussain <rakibhussain8812@gmail.com>
2025-02-13 13:03:22 +0530

    g

--- controllers/payments.js
+++ controllers/payments.js
@@ -7,6 +7,7 @@ const mailSender=require("../utils/mailsender")
 
 const {courseEnrollmentEmail}=require("../mail/templates/courseEnrollmentEmail")
 const { default: mongoose } = require("mongoose")
+const { default: webhooks } = require("razorpay/dist/types/webhooks")
 
 
 //capture the payment and initiate the razorpay order
@@ -91,6 +92,61 @@ exports.capturePayment=async(req,res)=>{
 
 
 //vertification signature of razorpay and server
+exports.verifySignature=async(req,res)=>{
+    //crated a webhook in backend 
+    const weebhookSecret="12345678"
 
+
+    const signature=req.headers["x-razorpay-signature"]
+
+    const shasum=crypto.createHmac("sha256",weebhookSecret)
+    shasum.update(JSON.stringify(req.body))
+    const digest=shasum.digest("hex");
+    if(signature===digest){
+        console.log("payment is authorized")
+
+        const {courseId,userId}=req.body.payload.payment.entity.notes
+
+        try {
+            //fulfil the action
+             
+            //find the course and enroll the student in it 
+            const  enrolledCourse=await Course.findOneAndUpdate({_id:courseId},{$push:{studentEnrolled:userId}},{new:true})
+            if(!enrolledCourse){
+                return res.status(404).json({
+                    success:false,
+                    message:"course not found"
+                })
+            }
+            console.log(enrolledCourse)
+
+
+            //find the student and update the schema by pushing the course in it 
+            const enrolledStudent=await User.findOneAndUpdate({_id:userId},{$push:{courses:courseId}},{new:true})
+
+            //mail send for confirmation 
+            const emailResponse=await mailSender(enrolledStudent.email,"Course enrollement","u are successfully enrolled into the course")
+
+            console.log(enrolledStudent)
+            return res.status(200).json({
+                success:true,
+                message:"student verified and enrolled successfully into the course",
+            })
+
+        } catch (error) {
+           return res.status(500).json({
+            success:false,
+            message:"some error while enrolment in the course"
+           }) 
+        }
+
+    }
+    else{
+        return res.status(500).json({
+            success:false,
+            message:"payment is unauthorized"
+        })
+    }
+}
 
 

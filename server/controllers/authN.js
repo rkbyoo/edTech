2b1fadf16ecc28590620e949f50d49b06ccda200
rakib hussain <rakibhussain8812@gmail.com>
2025-02-22 23:46:06 +0530

    streaks

--- server/controllers/authN.js
+++ server/controllers/authN.js
@@ -124,7 +124,7 @@ exports.signUp = async (req, res) => {
 			accountType,
 			approved,
 			additionalDetails: profileDetails._id,
-			image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+			image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName}${lastName}`,
 		});
 
 		return res.status(200).json({

adfede16555b8e4a4a1184f77f948aba7decb956
rakib hussain <rakibhussain8812@gmail.com>
2025-03-10 21:12:53 +0530

    g

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       {/* section1 */}
       <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between">
          <Link to={"/signup"}>
-            <div>
+            <div className="group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95">
                 <div className="flex flex-row items-center gap-4 mt-10 border-s-violet-200 rounded-sm">
                     <p>become an instructor</p>
                     <FaArrowRight></FaArrowRight>

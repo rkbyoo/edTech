3d3c31bf791d11173438b47cf5777e6cebdbdfea
rakib hussain <rakibhussain8812@gmail.com>
2025-03-12 13:21:14 +0530

    g

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import HighlightedText from "../components/core/HomePage/HighlightedText";
 import CTAButton from "../components/core/HomePage/CTAButton";
+import Banner from "../assets/Images/banner.mp4"
+
 
 const Home = () => {
   return (
     <div>
       {/* section1 */}
-      <div className="relative mx-auto flex flex-col w-11/12 items-center text-white justify-between">
+      <div className="relative max-w-max mx-auto flex flex-col w-11/12 items-center text-white justify-between">
          <Link to={"/signup"}>
             <div className="group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95">
                 <div className="flex flex-row items-center gap-4 border-s-violet-200 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
@@ -30,6 +32,14 @@ const Home = () => {
           <CTAButton active={true} linkto={"/singup"}>Learn more</CTAButton>
           <CTAButton active={false} linkto={"/login"}>Book a demo</CTAButton>
         </div>
+
+
+        <div className="mx-3 my-13 shadow-blue-200 w-[1080px] h-[600px]">
+          <video>
+          <source muted loop autoPlay src={Banner} className="rounded-md mt-14" type="video/mp4"></source>
+          </video>
+        </div>
+
       </div>
         
 
